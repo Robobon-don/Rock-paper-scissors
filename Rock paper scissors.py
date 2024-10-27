e6f017a9e6e6daeff880d72aff434927767b3896
Robobon-don <lox48126@gmail.com>
2024-10-27 20:23:27 +0300

    Update Rock paper scissors.py

--- Rock paper scissors.py
+++ Rock paper scissors.py
@@ -1,27 +1,27 @@
 import random
 
-gg = ["камень", "ножницы", "бумага"] #0,1,2
+gg = ["камень", "ножницы", "бумага"] 
 player = input('Вы выбрали: ')
 if player in gg:
     robot = random.sample(gg, 1)
     if player == robot[0]:
         print(f'Компьютер выбрал: {robot[0]}')
         print('Ничья!')
-    elif player in 'камень': #0,1
+    elif player in 'камень': 
         if robot[0] in "ножницы":
             print(f'Компьютер выбрал: {robot[0]}')
             print('Ты выйграл!')
         elif robot[0] in "бумага":
             print(f'Компьютер выбрал: {robot[0]}')
             print('Компьютер выиграл!')
-    elif player in "ножницы": #1,0
+    elif player in "ножницы": 
         if robot[0] in 'камень':
             print(f'Компьютер выбрал: {robot[0]}')
             print('Компьютер выиграл!')
         elif robot[0] in "бумага":
             print(f'Компьютер выбрал:{robot[0]}')
             print('Ты выйграл!')
-    elif player in "бумага": #2,0
+    elif player in "бумага": 
         if robot[0] in "ножницы":
             print(f'Компьютер выбрал: {robot[0]}')
             print('Компьютер выиграл!')
